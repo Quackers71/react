3e4b2b11b6812aff1fc68ac450602eeeabf3f1c3
Quackers <robq71@gmail.com>
2022-08-29 13:55:09 +0100

    rendering classes dynamically

--- demo/counter-app/src/components/counter.jsx
+++ demo/counter-app/src/components/counter.jsx
@@ -1,29 +1,29 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
-    state = {
-        count: 0,
-    };
+  state = {
+    count: 0
+  };
 
-    // You can use this styles property or inline as below
-    // styles = {
-    //   fontSize: 25,
-    //   fontWeight: "bold"
-    // }; 
+  render() {
+    return (
+      <React.Fragment>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </React.Fragment>
+    );
+  }
 
-    render() {
-      return (
-        <React.Fragment>
-          <span style={{ fontSize: 30, fontWeight: "bold" }} className="badge badge-primary m-2">{this.formatCount()}</span>
-          <button className="btn btn-secondary btn-sm">Increment</button>
-        </React.Fragment>
-      );
-    }
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+    classes += (this.state.count === 0) ? "warning" : "primary";
+    return classes;
+  }
 
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero ' : count;
-    }
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? 'Zero ' : count;
+  }
 }
  
 export default Counter;
