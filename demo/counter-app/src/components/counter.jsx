a9003a68c608244b823ad3425970820ba7dbfd28
Quackers <robq71@gmail.com>
2022-08-29 22:43:27 +0100

    passing Event Args using Arrow Function

--- demo/counter-app/src/components/counter.jsx
+++ demo/counter-app/src/components/counter.jsx
@@ -3,23 +3,28 @@ import React, { Component } from 'react';
 class Counter extends Component {
   state = {
     count: 0
-  };
+  }
 
   // constructor() {
   //   super();
   //   this.handleIncrement = this.handleIncrement.bind(this);
   // }
 
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product);
     this.setState({count: this.state.count + 1});
     // console.log("Increment Clicked", this);
   }
+
+  // doHandleIncrement = () => {
+  //   this.handleIncrement({id:1});
+  // }
 
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+        <button onClick={() => this.handleIncrement("product")} className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
