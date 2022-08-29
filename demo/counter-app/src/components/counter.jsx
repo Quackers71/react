ee2b3f2de1134397cbc341423af05fc4b113a025
Quackers <robq71@gmail.com>
2022-08-29 09:53:32 +0100

    using styles property or inline style

--- demo/counter-app/src/components/counter.jsx
+++ demo/counter-app/src/components/counter.jsx
@@ -3,15 +3,19 @@ import React, { Component } from 'react';
 class Counter extends Component {
     state = {
         count: 0,
-        imageURL: 'https://picsum.photos/200'
     };
+
+    // You can use this styles property or inline as below
+    // styles = {
+    //   fontSize: 25,
+    //   fontWeight: "bold"
+    // }; 
 
     render() {
       return (
         <React.Fragment>
-          <img src={this.state.imageURL}/>
-          <span>{this.formatCount()}</span>
-          <button>Increment</button>
+          <span style={{ fontSize: 30, fontWeight: "bold" }} className="badge badge-primary m-2">{this.formatCount()}</span>
+          <button className="btn btn-secondary btn-sm">Increment</button>
         </React.Fragment>
       );
     }
