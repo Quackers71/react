f3e4e5997b7912929a1fe0bbf0799785b24403b9
Quackers <robq71@gmail.com>
2022-08-30 23:28:39 +0100

    raising and handling events

--- demo/counter-app/src/components/counters.jsx
+++ demo/counter-app/src/components/counters.jsx
@@ -2,25 +2,28 @@ import React, { Component } from 'react';
 import Counter from './counter'
 
 class Counters extends Component {
-    state = { 
-      counters: [
-        { id:1, value: 0},
-        { id:2, value: 0},
-        { id:3, value: 0},
-        { id:4, value: 0}
-      ]
-    };
+  state = { 
+    counters: [
+      { id:1, value: 0},
+      { id:2, value: 0},
+      { id:3, value: 0},
+      { id:4, value: 0}
+    ]
+  };
 
-    render() {
+  handleDelete = () => {
+    console.log("Event Handler Called");
+  };
 
-      return (
+  render() {
+    return (
       <div>
         {this.state.counters.map(counter => (
-        <Counter key={counter.id} value={counter.value} />
+        <Counter key={counter.id} onDelete={this.handleDelete} value={counter.value} id={counter.id} />
         ))}
       </div>
-      );
-    };
+    );
+  };
 };
  
 export default Counters;
